f5635c77edac4639bb4ff1cd99900269ee6f5df5
Matt <mrnewbigging@hotmail.com>
2017-11-28 19:54:38 +0000

    adding comments

--- PrimeNumberGen/Eratosthenes.cpp
+++ PrimeNumberGen/Eratosthenes.cpp
@@ -8,26 +8,30 @@ void Eratosthenes::Sieve()
 {
 	// Prep primes bool list
 	// Uses this class' n value for limit (set in constructor)
+	// (1n) Good place to make parallel
 	for (int i = 0; i < limit + 1; i++)
 	{
 		primes.push_back(true);
 	}
 
 	// Run
+	// (?n) 
 	for (int p = 2; p*p <= limit; p++)
 	{
-		// If prime[p] is not changed, it's a prime
+		// If prime[p] is not checked/prime
 		if (primes[p])
 		{
 			// Update all multiples of p
 			for (int i = p * 2; i <= limit; i += p)
 			{
+				// This is not a prime
 				primes[i] = false;
 			}
 		}
 	} // end run loop
 
 	// Turn bools list into prime numbers result list
+	// (2n)
 	for (int p = 2; p <= limit; p++)
 	{
 		if (primes[p])
